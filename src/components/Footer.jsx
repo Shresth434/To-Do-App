0ef764b79f7f9c7ae50b366b408798b52a9fc9c2
Shresth434 <shresthkatiyar111@gmail.com>
2026-03-27 19:50:36 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer({ CompletedTodos, TotalTodos }) {
   return (
-    <div className="bg-yellow-400 text-red-500  shadow-[0_-3px_14px_rgba(177,120,218,1)] text-3xl font-semibold font-serif fixed bottom-0 w-full flex justify-evenly py-5">
+    <div className="bg-violet-500 text-white  shadow-[0_-3px_14px_rgba(177,120,218,1)] text-3xl font-semibold font-serif fixed bottom-0 w-full flex justify-evenly py-5">
       <span>Completed Todos : {CompletedTodos}</span>
       <span>Total Todos : {TotalTodos}</span>
     </div>
