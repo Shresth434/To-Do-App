e746be87c1031fd0dffe784d6825f9ff62c44d5c
Shresth434 <shresthkatiyar111@gmail.com>
2026-03-27 19:48:41 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <h1 className="bg-purple-500 text-white text-center py-3 shadow-[0_3px_14px_rgba(177,120,218,1)] text-5xl font-semibold font-serif">
+    <h1 className="bg-yellow-400 text-red-500 text-center py-3 shadow-[0_3px_14px_rgba(177,120,218,1)] text-5xl font-semibold font-serif">
       My Todo List
     </h1>
   );
